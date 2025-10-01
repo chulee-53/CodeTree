18d9d1af09806c1243fe12a15be94eae1cfc651b
Awesome/Lee seung chul <chulee503@naver.com>
2025-10-01 18:20:51 +0900

    Title: 달에서 무게 구하기, Time: 104ms, Memory: 9MB, Status: Passed - Codetree

---
+++ 251001/달에서 무게 구하기/weight-on-the-moon.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        int chu = 13;
+        double moon = 0.165;
+        System.out.printf("%d * %.6f = %.6f", chu, moon, chu*moon);
+    }
+}
