8de8c3c1bf3a71d0a59422bcf1aa3407ba4ee8e1
Leeseungchul <chulee503@naver.com>
2026-03-12 18:05:48 +0900

    Title: 합과 평균, Time: 84ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 260312/합과 평균/sum-and-avg.js
@@ -0,0 +1,9 @@
+const fs = require("fs");
+let input = fs.readFileSync(0).toString().trim().split(" ");
+
+let a = Number(input[0]);
+let b = Number(input[1]);
+
+let average = (a + b) / 2;
+
+console.log(a + b, average.toFixed(1));
